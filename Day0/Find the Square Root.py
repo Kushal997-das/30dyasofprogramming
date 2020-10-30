a0d3b8ce1a7d40840fb847352762a768def22289
Kushal Das <daskushal980@gmail.com>
2020-10-30 08:45:26 +0530

    Create Find the Square Root.py

---
+++ Day0/Find the Square Root.py
@@ -0,0 +1,8 @@
+# Python Program to calculate the square root
+
+
+# To take the input from the user
+num = float())
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
